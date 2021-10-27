df278d0b6f0578bee27699f8ae595ffb1dc09111
Lester Cheong <s10208233@connect.np.edu.sg>
2021-10-27 11:18:31 +0800

    Add project files.

---
+++ ocbc-team1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ocbc_team1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
