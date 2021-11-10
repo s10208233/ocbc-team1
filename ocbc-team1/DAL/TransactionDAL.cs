c72a8a6723088244f03c2e492e0de471feabfeb9
Lester Cheong <s10208233@connect.np.edu.sg>
2021-11-10 11:34:58 +0800

    transactionDAL prep

---
+++ ocbc-team1/DAL/TransactionDAL.cs
@@ -0,0 +1,38 @@
+﻿using ocbc_team1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ocbc_team1.DAL
+{
+    public class TransactionDAL
+    {
+        LoginDAL loginContext = new LoginDAL();
+        public List<BankAccount> getBankAccountList(string accesscode) 
+        {
+            List<User> userlist = loginContext.retrieveUserList() ;
+            foreach (User u in userlist) 
+            {
+                if (u.AccessCode == accesscode) 
+                {
+                    return u.AccountsList;
+                }
+            }
+            return null;
+        }
+
+        public List<Transaction> getTransactionList(string accesscode)
+        {
+            List<User> userlist = loginContext.retrieveUserList();
+            foreach (User u in userlist)
+            {
+                if (u.AccessCode == accesscode)
+                {
+                    return u.TransactionList;
+                }
+            }
+            return null;
+        }
+    }
+}
