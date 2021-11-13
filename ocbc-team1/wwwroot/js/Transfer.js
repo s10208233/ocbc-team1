dcae975b5631022f35bf5c68d8584f63f8f47222
lincolnchia <73088199+lincolnchia@users.noreply.github.com>
2021-11-13 14:43:39 +0800

    adding of javascript to hide or show the transfer pages

---
+++ ocbc-team1/wwwroot/js/Transfer.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function changingOptions() {
+    var x = document.getElementById("PhoneNumber")
+    var y = document.getElementById("AccountNumber")
+    if (y.style.display = "none") {
+        y.style.display = "block";
+        if (x.style.display = "block") {
+            x.style.display = "none";
+        }
+    }
+}
+
+function changingOptions2() {
+    var x = document.getElementById("PhoneNumber")
+    var y = document.getElementById("AccountNumber")
+    if (x.style.display = "none") {
+        x.style.display = "block";
+        if (y.style.display = "block") {
+            y.style.display = "none";
+        }
+    }
+    
+}
