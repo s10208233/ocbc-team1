d16ede781fbe18190c9cb1c94e276d9e3980ffd6
lincolnchia <73088199+lincolnchia@users.noreply.github.com>
2021-11-15 22:54:17 +0800

    Making of transfer History table

--- ocbc-team1/Controllers/DashboardController.cs
+++ ocbc-team1/Controllers/DashboardController.cs
@@ -21,9 +21,9 @@ namespace ocbc_team1.Controllers
         
         public IActionResult TransferHistory(int AccountNumber)
         {
-            int accNo = AccountNumber;
-            ViewData["AccountNo"] = accNo;
-            return View();
+            string accesscode = HttpContext.Session.GetString("accesscode");
+            List<Transaction> transactionList = transactionContext.getTransactionList(accesscode);
+            return View(transactionList);
         }
 
         public IActionResult Transfer()  
