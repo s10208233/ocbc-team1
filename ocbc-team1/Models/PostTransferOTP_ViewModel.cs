a18a633add8d3fc4c6e708c4cff053820cca53d3
Lester Cheong <s10208233@connect.np.edu.sg>
2021-11-17 17:38:15 +0800

    Newpage vm

---
+++ ocbc-team1/Models/PostTransferOTP_ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ocbc_team1.Models
+{
+    public class PostTransferOTP_ViewModel
+    {
+        [Required]
+        public TransferViewModel tfvm { get; set; }
+
+        [Required]
+        public string OTP { get; set; }
+    }
+}
