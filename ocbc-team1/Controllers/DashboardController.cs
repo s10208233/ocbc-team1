d23b08c4f4c40c3cdaf3b0027f1a4702c7c9820a
SuperNovaV2 <s10205505@connect.np.edu.sg>
2021-11-17 17:39:03 +0800

    otp for transfer

--- ocbc-team1/Controllers/DashboardController.cs
+++ ocbc-team1/Controllers/DashboardController.cs
@@ -6,13 +6,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace ocbc_team1.Controllers
 {
     public class DashboardController : Controller
     {
+        static TelegramBotClient Bot = new TelegramBotClient("2106855009:AAEVAKqEbNj6W7GeZoOLkgmF8XgsL7ZvG2o");
         private TransactionDAL transactionContext = new TransactionDAL();
         private NewBankAccountDAL newaccountContext = new NewBankAccountDAL();
+        private TelegramDAL teleContext = new TelegramDAL();
         public IActionResult Index()
         {
             string accesscode = HttpContext.Session.GetString("accesscode");
@@ -31,18 +34,41 @@ namespace ocbc_team1.Controllers
 
         public IActionResult Transfer()  
         {
+            
             ViewData["TransferType"] = new List<string> { "Using OCBC Acount Number", "Using Phone Number" };
             ViewData["UserOwnAccountList"] = transactionContext.getBankAccountList(HttpContext.Session.GetString("accesscode"));
             return View();
         }
 
+        public IActionResult transferOTP()
+        {
+            string accesscode = HttpContext.Session.GetString("accesscode");
+            Random rnd = new Random();
+            string rOTP = Convert.ToString(rnd.Next(000000, 999999));
+            HttpContext.Session.SetString("otp", rOTP);
+            string text = "Your OTP is: " + rOTP;
+            if (teleContext.getTelegramChatId(accesscode) != null)
+            {
+                string chatid = Convert.ToString(teleContext.getTelegramChatId(accesscode));
+                sendMessage(chatid, text);
+
+            }
+            return View();
+        }
+        public async Task sendMessage(string destID, string text)
+        {
+            await Bot.SendTextMessageAsync(destID, text);
+
+        }
+
         [HttpPost]
         public IActionResult CreateTransfer(TransferViewModel tfViewModel) 
         {
-            //if (tfViewModel.From_AccountNumber == null)
-            //{
-            //    TempData["ErrorMessage"] = "Please enter a bank account number to transfer to"; 
-            //}
+            if(tfViewModel.OTP != HttpContext.Session.GetString("otp"))
+            {
+                TempData["ErrorMessage"] = "Invalid OTP";
+                return RedirectToAction("Transfer", "Dashboard", tfViewModel);
+            }
             if (transactionContext.checkRecipient(tfViewModel) == false)
             {
                 TempData["ErrorMessage"] = "Recipient Doesn't exsist please try again";
