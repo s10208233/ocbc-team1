0c487879fdd9e8373d70e5dd0109a4480b1accce
SuperNovaV2 <s10205505@connect.np.edu.sg>
2022-01-14 10:12:58 +0800

    Using Twillio API To Integrate SMS as an option for OTP and notifications.

---
+++ ocbc-team1/Models/ProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ocbc_team1.Models
+{
+    public class ProfileViewModel
+    {
+        public string? TypeOTP { get; set; }
+    }
+}
